41f16657ec26be2f3959954050a5f9a6c5ec19fb
Gustavo Bugs <gustavokbugs@gmail.com>
2023-07-16 16:51:03 -0300

    washington dc test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const horas = document.getElementById('horas')
 const minutos = document.getElementById('minutos')
 const segundos = document.getElementById('segundos')
+const horas_us = document.getElementById('horas-us')
+const minutos_us = document.getElementById('minutos-us')
+const segundos_us = document.getElementById('segundos-us')
+
 
 const relogio = setInterval(function time() {
     let dateToday = new Date();
@@ -17,4 +21,21 @@ const relogio = setInterval(function time() {
     horas.textContent = hr;
     minutos.textContent = min
     segundos.textContent = s
+})
+
+const relogio_us = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours() -1;
+    let min = dateToday.getMinutes();
+    let s = dateToday.getSeconds();
+
+    if (hr < 10) hr = '0' + hr
+
+    if (min < 10) min = '0' + min
+
+    if (s < 10) s = '0' + s
+
+    horas_us.textContent = hr;
+    minutos_us.textContent = min
+    segundos_us.textContent = s
 })
